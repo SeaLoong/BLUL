18d79cb7eb36e92c131275e2682757274d0ed596
SeaLoong <984391132@qq.com>
2020-07-25 00:00:48 +0800

    Update apptoken.js

--- src/modules/apptoken.js
+++ src/modules/apptoken.js
@@ -206,6 +206,9 @@ export default async function (importModule, BLUL, GM) {
   });
 
   BLUL.AppToken = {
+    get headers () {
+      return headers;
+    },
     sign,
     getNewAccessToken,
     getAccessToken
