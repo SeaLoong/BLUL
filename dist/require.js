12ec3f8a83c50082025b87d58f2abefac18bd3ba
SeaLoong <SeaLoong@users.noreply.github.com>
2020-07-24 14:10:03 +0000

    Automatic commit the generated files
    Signed-off-by: GitHub Actions <actions@github.com>

--- dist/require.js
+++ dist/require.js
@@ -86,6 +86,7 @@ BLUL.createImportModuleFunc = function (context, keepContext = false) {
         importUrlMap.set(name, ret);
         return ret;
       } catch (error) {
+        (BLUL.Logger ?? console).error('模块导入失败，尝试使用script标签加载', error);
         return new Promise((resolve, reject) => {
           const elem = document.createElement('script');
           elem.onerror = reject;
@@ -98,7 +99,6 @@ BLUL.createImportModuleFunc = function (context, keepContext = false) {
         });
       }
     } catch (error) {
-      (BLUL.Toast ?? console).error('模块导入失败', error);
     }
   }
   if (!keepContext) context.unshift(importModule);
@@ -124,7 +124,7 @@ BLUL.createImportModuleFromCodeFunc = function (context, keepContext = false) {
       importCodeMap.set(code, ret);
       return ret;
     } catch (error) {
-      (BLUL.Toast ?? console).error('模块导入失败', error);
+      (BLUL.Logger ?? console).error('模块导入失败', error);
     }
   }
   if (!keepContext) context.unshift(importModule);
