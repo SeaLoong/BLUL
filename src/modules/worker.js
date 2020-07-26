e7437ab6d28f30c139a3b5ab96da2041cf9a6188
SeaLoong <984391132@qq.com>
2020-07-26 19:36:01 +0800

    update

--- src/modules/worker.js
+++ src/modules/worker.js
@@ -1,42 +1,44 @@
 export default async function (importModule, BLUL, GM) {
   const Util = BLUL.Util;
 
-  let worker;
-  try {
-    worker = new Worker(Util.codeToURL(`importScripts('${await BLUL.getResourceUrl('Worker/env')}');`), { type: 'classic', credentials: 'include', name: BLUL.NAME + '-Worker' });
-  } catch (error) {
-    BLUL.Logger.error('Worker加载失败，请检查是否出现插件冲突', '已知冲突的插件有:', 'pakku：哔哩哔哩弹幕过滤器', error);
-    return;
-  }
+  let channel;
+  const init = async () => {
+    try {
+      const worker = new Worker(Util.codeToURL(`importScripts('${await BLUL.getResourceUrl('Worker/env')}');`), { type: 'classic', credentials: 'include', name: BLUL.NAME + '-Worker' });
 
-  const initUrlMap = new Map();
+      const initUrlMap = new Map();
+      worker.onerror = worker.onmessageerror = e => BLUL.Logger.error('Worker执行时出现错误', e);
+      worker.onmessage = async e => {
+        if (!(e.data instanceof Array) || e.data.length < 1) return;
+        if (initUrlMap.has(e.data[0])) {
+          const resolve = initUrlMap.get(e.data[0]);
+          initUrlMap.delete(e.data[0]);
+          resolve();
+        }
+      };
 
-  worker.onerror = worker.onmessageerror = e => BLUL.Logger.error('Worker执行时出现错误', e);
+      const initImport = async (name, op = 'IMPORT') => {
+        const url = await BLUL.getResourceUrl(name);
+        worker.postMessage([op, url]);
+        return new Promise(resolve => initUrlMap.set(url, resolve));
+      };
 
-  worker.onmessage = async e => {
-    if (!(e.data instanceof Array) || e.data.length < 1) return;
-    if (initUrlMap.has(e.data[0])) {
-      const resolve = initUrlMap.get(e.data[0]);
-      initUrlMap.delete(e.data[0]);
-      resolve();
+      await initImport('lodash');
+      await initImport('Worker/channel', 'CHANNEL');
+
+      channel = new (await importModule('Worker/channel'))(worker);
+      channel.env.push(BLUL, GM);
+      await channel.postENV(BLUL, GM);
+    } catch (error) {
+      BLUL.Logger.error('Worker加载失败，请检查是否出现插件冲突', '已知冲突的插件有:', 'pakku：哔哩哔哩弹幕过滤器', error);
     }
   };
 
-  const initImport = async (name, op = 'IMPORT') => {
-    const url = await BLUL.getResourceUrl(name);
-    worker.postMessage([op, url]);
-    return new Promise(resolve => initUrlMap.set(url, resolve));
-  };
-
-  await initImport('lodash');
-  await initImport('Worker/channel', 'CHANNEL');
-
-  const channel = new (await importModule('Worker/channel'))(worker);
-  channel.env.push(BLUL, GM);
-  await channel.postENV(BLUL, GM);
-
   BLUL.Worker = {
     importModule: async (name) => {
+      if (!channel) {
+        await init();
+      }
       return channel.postIMPORT(await BLUL.getResourceUrl(name));
     }
   };
